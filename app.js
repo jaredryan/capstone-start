d39c0aa03db8ed64f9df25b745db4763aa275cbb
Jared Ryan <jryantennis@gmail.com>
2017-07-21 17:17:06 -0700

    slowly moving

--- app.js
+++ app.js
@@ -1,42 +1,97 @@
-var NETFLIX_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-
-
 var RESULT_HTML_TEMPLATE = (
   '<div class="result">' +
-      // Insert relevant content that I want to display
+      '<a class="js-result-image" href="">Video?</a>' + 
+      '<p class="js-result-title"></p>' + 
+      '<p class="js-result-type"></p>' + 
+      '<p class="js-result-teaser"></p>' + 
+      '<a class="js-result-wURL" href="">Learn More</a>' + 
   '</div>'
 );
+
+
+// function getDataFromKeywordApi(searchTerm, callback) {
+//   var query = {
+//     q: searchTerm,
+//     info: 1,
+//     k: "277746-TasteTex-TMP1RQ9L"
+//     // Insert relevant parameters here
+//   }
+//   $.getJSON(NETFLIX_SEARCH_URL, query, callback);
+// }
 
 function getDataFromApi(searchTerm, callback) {
   var query = {
     q: searchTerm,
+    key: "QS2J66v7DgzghRrE21wvg"
     // Insert relevant parameters here
   }
-  $.getJSON(NETFLIX_SEARCH_URL, query, callback);
+  $.ajax({
+    url: "https://www.goodreads.com/search/index.xml", 
+    data: query, 
+    dataType: "jsonp",
+    success: callback
+  });
 }
+
+// function getDataFromDirectorApi(searchTerm, callback) {
+//   var query = {
+//     q: searchTerm,
+//     info: 1,
+//     k: "277746-TasteTex-TMP1RQ9L"
+//     // Insert relevant parameters here
+//   }
+//   $.getJSON(NETFLIX_SEARCH_URL, query, callback);
+// }
+
 
 
 function renderResult(result) {
-  
   var template = $(RESULT_HTML_TEMPLATE);
-  // Alter what needs to be altered
-  
+
+  template.find(".js-result-image").attr("href", result.yURL);
+  template.find(".js-result-title").text(result.Name);
+  template.find(".js-result-type").text(result.Type);
+  template.find(".js-result-teaser").text(result.wTeaser);
+  template.find(".js-result-wURL").attr("href", result.wURL);
   return template;
 }
 
+// Done
 function displaySearchData(data) {
-  var results = data.items.map(function(item, index) {
-     return renderResult(item);
-  });
-  $('.js-rec-list').html(results);
+  console.log(data);
+  // var results = data.Similar.Results.map(function(item) {
+  //    return renderResult(item);
+  // });
+  // $('.js-rec-list').html(results);
 }
 
 
 // This is fine
-function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
-    event.preventDefault();
-    var queryTarget = $(event.currentTarget).find('.js-query');
+// function watchKeyword() {
+//   $('.keyword').click(function(event) {
+//     var queryTarget = $('.js-query');
+//     var query = queryTarget.val();
+//     // clear out the input
+//     queryTarget.val("");
+//     // ALL OF THE ABOVE WILL REMAIN THE SAME
+//     getDataFromKeywordApi(query, displaySearchData);
+//   });
+// }
+
+// function watchDirector() {
+//   $('.director').click(function(event) {
+//     var queryTarget = $('.js-query');
+//     var query = queryTarget.val();
+//     // clear out the input
+//     queryTarget.val("");
+//     // ALL OF THE ABOVE WILL REMAIN THE SAME
+//     getDataFromDirectorApi(query, displaySearchData);
+//   });
+// }
+
+function watchSearch() {
+  $('.search-button').click(function(event) {
+    var queryTarget = $('.js-query');
     var query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
@@ -47,8 +102,12 @@ function watchSubmit() {
 
 // This is also fine
 $(function() {
-  watchSubmit();
-  watchAddtoList();
+  // watchClick();
+  watchSearch();
+  // watchDirector();
+  // watchKeyword();
+  // watchAddtoList();
+
   // Need to do something that will activate the jumping mechanism?
   // Need to do something to watch for Previous and Next
 });
